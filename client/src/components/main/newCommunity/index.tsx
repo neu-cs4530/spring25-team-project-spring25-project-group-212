a6cfbe36242becac4089f407a05b9d140013efa1
K-maker411 <chiefkash2004@gmail.com>
2025-03-05 17:18:29 -0600

    Application works fine if i comment out newCommunity index.tsx, trying to fix

--- client/src/components/main/newCommunity/index.tsx
+++ client/src/components/main/newCommunity/index.tsx
@@ -1,59 +1,59 @@
-import useNewCommunity from '../../../hooks/useNewCommunity';
+// import useNewCommunity from '../../../hooks/useNewCommunity';
 
-const NewCommunityPage = () => {
-  const {
-    name,
-    setName,
-    about,
-    setAbout,
-    rules,
-    setRules,
-    nameErr,
-    aboutErr,
-    rulesErr,
-    postCommunity,
-    err,
-  } = useNewCommunity();
+// const NewCommunityPage = () => {
+//   const {
+//     name,
+//     setName,
+//     about,
+//     setAbout,
+//     rules,
+//     setRules,
+//     nameErr,
+//     aboutErr,
+//     rulesErr,
+//     postCommunity,
+//     err,
+//   } = useNewCommunity();
 
-  const isNameErr = nameErr !== '';
-  const isAboutErr = aboutErr !== '';
-  const isRulesErr = rulesErr !== '';
-  return (
-    <form>
-      <input
-        id='formNameInput'
-        value={name}
-        onChange={e => setName(e.target.value)}
-        onError={isNameErr}
-        helperText={nameErr}
-        label='Enter Community Name'
-      />
-      <TextField
-        id='formDescriptionInput'
-        value={about}
-        onChange={e => setAbout(e.target.value)}
-        error={isAboutErr}
-        helperText={aboutErr}
-        label="Enter Community 'About' Text"
-      />
-      <TextField
-        id='formRulesInput'
-        value={rules}
-        onChange={e => setRules(e.target.value)}
-        error={isRulesErr}
-        helperText={rulesErr}
-        label='Enter Community Rules'
-      />
-      <Button
-        variant='contained'
-        color='primary'
-        onClick={() => {
-          postCommunity();
-        }}>
-        Create Community
-      </Button>
-    </form>
-  );
-};
+//   const isNameErr = nameErr !== '';
+//   const isAboutErr = aboutErr !== '';
+//   const isRulesErr = rulesErr !== '';
+//   return (
+//     <form>
+//       <input
+//         id='formNameInput'
+//         value={name}
+//         onChange={e => setName(e.target.value)}
+//         onError={isNameErr}
+//         helperText={nameErr}
+//         label='Enter Community Name'
+//       />
+//       <TextField
+//         id='formDescriptionInput'
+//         value={about}
+//         onChange={e => setAbout(e.target.value)}
+//         error={isAboutErr}
+//         helperText={aboutErr}
+//         label="Enter Community 'About' Text"
+//       />
+//       <TextField
+//         id='formRulesInput'
+//         value={rules}
+//         onChange={e => setRules(e.target.value)}
+//         error={isRulesErr}
+//         helperText={rulesErr}
+//         label='Enter Community Rules'
+//       />
+//       <Button
+//         variant='contained'
+//         color='primary'
+//         onClick={() => {
+//           postCommunity();
+//         }}>
+//         Create Community
+//       </Button>
+//     </form>
+//   );
+// };
 
-export default NewCommunityPage;
+// export default NewCommunityPage;
