7e58986fff488fdad472ff75473f636d04b3609e
Sumer Shinde <sumershinde22@gmail.com>
2025-03-08 17:08:58 -0500

    added frontend for markdown and markdown preview when adding question/answer"

--- client/src/components/main/newAnswer/index.tsx
+++ client/src/components/main/newAnswer/index.tsx
@@ -1,5 +1,6 @@
 import './index.css';
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import Form from '../baseComponents/form';
 import TextArea from '../baseComponents/textarea';
 import useAnswerForm from '../../../hooks/useAnswerForm';
@@ -9,6 +10,7 @@ import useAnswerForm from '../../../hooks/useAnswerForm';
  */
 const NewAnswerPage = () => {
   const { text, textErr, setText, postAnswer } = useAnswerForm();
+  const [useMarkdown, setUseMarkdown] = useState(false);
 
   return (
     <Form>
@@ -19,6 +21,24 @@ const NewAnswerPage = () => {
         setState={setText}
         err={textErr}
       />
+      <div className='toggle-container'>
+        <label>
+          <input
+            type='checkbox'
+            checked={useMarkdown}
+            onChange={() => setUseMarkdown(!useMarkdown)}
+          />
+          Enable Markdown
+        </label>
+      </div>
+      {useMarkdown && text && (
+        <div className='markdown-preview' style={{ marginBottom: '20px' }}>
+          <h3>Markdown Preview:</h3>
+          <div className='markdown-box'>
+            <ReactMarkdown>{text}</ReactMarkdown>
+          </div>
+        </div>
+      )}
       <div className='btn_indicator_container'>
         <button className='form_postBtn' onClick={postAnswer}>
           Post Answer
