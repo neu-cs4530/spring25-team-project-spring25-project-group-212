bfb7e26db7824d0626778dd245444edfddb473b3
Immanuel Dorneich <immanuel.dorneich@gmail.com>
2025-03-09 20:11:07 -0400

    save unsave buttons and functionality

--- client/src/components/main/answerPage/questionBody/index.tsx
+++ client/src/components/main/answerPage/questionBody/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.css';
 import { handleHyperlink } from '../../../../tool';
-
+import useUserContext from '../../../../hooks/useUserContext';
+import useQuestion from '../../../../hooks/useQuestion';
 /**
  * Interface representing the props for the QuestionBody component.
  *
@@ -9,12 +10,14 @@ import { handleHyperlink } from '../../../../tool';
  * - text - The content of the question, which may contain hyperlinks.
  * - askby - The username of the user who asked the question.
  * - meta - Additional metadata related to the question, such as the date and time it was asked.
+ * - qid - String representing the ObjectId of the question.
  */
 interface QuestionBodyProps {
   views: number;
   text: string;
   askby: string;
   meta: string;
+  qid: string;
 }
 
 /**
@@ -26,16 +29,39 @@ interface QuestionBodyProps {
  * @param text The content of the question.
  * @param askby The username of the question's author.
  * @param meta Additional metadata related to the question.
+ * @param qid String representing the ObjectId of the question.
  */
-const QuestionBody = ({ views, text, askby, meta }: QuestionBodyProps) => (
-  <div id='questionBody' className='questionBody right_padding'>
-    <div className='bold_title answer_question_view'>{views} views</div>
-    <div className='answer_question_text'>{handleHyperlink(text)}</div>
-    <div className='answer_question_right'>
-      <div className='question_author'>{askby}</div>
-      <div className='answer_question_meta'>asked {meta}</div>
+const QuestionBody = ({ views, text, askby, meta, qid }: QuestionBodyProps) => {
+  const { user: currentUser } = useUserContext();
+  const { handleToggleSaveQuestion, handleSetQuestionSaved, questionSaved } = useQuestion();
+  useEffect(() => {
+    handleSetQuestionSaved(currentUser.username, qid);
+  }, [currentUser.username, qid, handleSetQuestionSaved]);
+  return (
+    <div id='questionBody' className='questionBody right_padding'>
+      <div className='bold_title answer_question_view'>{views} views</div>
+      <div className='answer_question_text'>{handleHyperlink(text)}</div>
+      <div className='answer_question_right'>
+        <div className='question_author'>{askby}</div>
+        <div className='answer_question_meta'>asked {meta}</div>
+      </div>
+      <div>
+        {questionSaved ? (
+          <button
+            className='btn'
+            onClick={() => handleToggleSaveQuestion(currentUser.username, qid)}>
+            Unsave
+          </button>
+        ) : (
+          <button
+            className='btn'
+            onClick={() => handleToggleSaveQuestion(currentUser.username, qid)}>
+            Save
+          </button>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default QuestionBody;
