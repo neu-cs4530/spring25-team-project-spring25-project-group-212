243a96b4e49b5db31ce765833c2580d68d50ad99
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2025-04-05 12:39:04 -0400

    small changes to remove lines

--- server/controllers/chat.controller.ts
+++ server/controllers/chat.controller.ts
@@ -268,11 +268,6 @@ const chatController = (socket: FakeSOSocket) => {
     try {
       const updatedChat = await renameChat(chatId, newName);
 
-      // if (!updatedChat) {
-      //   res.status(404).send('Chat not found');
-      //   return;
-      // }
-
       socket.emit('chatUpdate', { chat: updatedChat, type: 'renamed' });
       res.json(updatedChat);
     } catch (err) {
