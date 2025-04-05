78f9adc295de0c82ad55913b170dae5dea51907c
K-maker411 <chiefkash2004@gmail.com>
2025-04-05 15:52:49 -0400

    Fixed auto-join issue for previews

--- client/src/components/main/communitiesListPage/communitiesListCardJoin/index.tsx
+++ client/src/components/main/communitiesListPage/communitiesListCardJoin/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PopulatedDatabaseCommunity } from '@fake-stack-overflow/shared';
+import { useNavigate } from 'react-router-dom';
 
 const CommunitiesListCardJoin = ({
   community,
@@ -9,19 +10,26 @@ const CommunitiesListCardJoin = ({
   community: PopulatedDatabaseCommunity;
   handleCommunityJoin: (communityId: string | undefined) => void;
   handleCommunityPreview: (communityId: string | undefined) => void;
-}) => (
-  <div>
-    <p>
-      <strong>Community Name: </strong> {community.name}
-    </p>
-    <p>
-      <strong>Community Rules: </strong> {community.rules}
-    </p>
-    <button onClick={() => handleCommunityJoin(community._id.toString())}>Join Community</button>
-    <button onClick={() => handleCommunityPreview(community._id.toString())}>
-      Preview Community
-    </button>
-  </div>
-);
-
+}) => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <p>
+        <strong>Community Name: </strong> {community.name}
+      </p>
+      <p>
+        <strong>Community Rules: </strong> {community.rules}
+      </p>
+      <button onClick={() => handleCommunityJoin(community._id.toString())}>Join Community</button>
+      <button
+        onClick={() =>
+          navigate(`/community/${community._id.toString()}`, {
+            state: { isPreview: true }, // Pass `isPreview` state
+          })
+        }>
+        Preview Community
+      </button>
+    </div>
+  );
+};
 export default CommunitiesListCardJoin;
